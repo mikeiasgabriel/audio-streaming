33f69d065343532a5297a031aa810a6522e4defe
Mikeias Gabriel <contato.mikeiasgabriel@gmail.com>
2021-10-29 19:36:42 -0300

    refactor: add logic to play anothers sounds

--- server/index.js
+++ server/index.js
@@ -16,18 +16,31 @@ const scdl = scdlCreate({
   axiosInstance: axios.create()
 })
 
+const sounds = {
+  motor_grafico: 'https://soundcloud.com/user-746329428/motores-graficos-de-jogos/s-V4WP2fkzc3s?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV',
+  design_patterns: 'https://soundcloud.com/user-746329428/design-patterns/s-ZvRa9qzDYei?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV',
+  direct: 'https://soundcloud.com/user-746329428/directcast/s-BrTV8T0D7Kl?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV',
+  dev_cast: 'https://soundcloud.com/user-746329428/devcast/s-0AcVucpLys3?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV',
+}
+
 http
   .createServer(async function (req, res) {
     const { url, method } = req;
     
-    const audioStream = await scdl.download(SOUNDCLOUD_URL);
+    const soundToListen = sounds[url];
     
-
-    res.writeHead(200, { "Content-Type": "audio/mpeg" });
-    audioStream.on("data", (data) => {
-      //debug(data);
-      res.write(data);
-    });
+    
+    if(soundToListen) {
+      debug(`request to listen ${url}`)
+      
+      const audioStream = await scdl.download(soundToListen);
+    
+      res.writeHead(200, { "Content-Type": "audio/mpeg" });
+      audioStream.on("data", (data) => {
+        //debug(data);
+        res.write(data);
+      });
+    }
   })
   .listen(port, host);
 
