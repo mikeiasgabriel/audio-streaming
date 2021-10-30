e66cddc1110f7bcd7374acc04b378b6882292df3
Ascênio Sanderson <ascenio@hotmail.com>
2021-10-29 21:16:15 -0300

    style: format code

--- server/index.js
+++ server/index.js
@@ -2,40 +2,43 @@ const http = require("http");
 const port = 8888;
 const host = "localhost";
 
-const scdlCreate = require('soundcloud-downloader').create;
-const axios = require('axios').default
-const debug = require('debug')('http')
+const scdlCreate = require("soundcloud-downloader").create;
+const axios = require("axios").default;
+const debug = require("debug")("http");
 
-const CLIENT_ID = this.CLIENT_ID || 'yGulQnndmijePkC8RlDVWKoK8FMaMUnk'
+const CLIENT_ID = this.CLIENT_ID || "yGulQnndmijePkC8RlDVWKoK8FMaMUnk";
 
 const scdl = scdlCreate({
   clientID: CLIENT_ID,
   saveClientID: true,
-  filePath: './client_id.json',
-  axiosInstance: axios.create()
-})
+  filePath: "./client_id.json",
+  axiosInstance: axios.create(),
+});
 
 const sounds = {
-  motor_grafico: 'https://soundcloud.com/user-746329428/motores-graficos-de-jogos/s-V4WP2fkzc3s?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV',
-  design_patterns: 'https://soundcloud.com/user-746329428/design-patterns/s-ZvRa9qzDYei?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV',
-  direct: 'https://soundcloud.com/user-746329428/directcast/s-BrTV8T0D7Kl?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV',
-  dev_cast: 'https://soundcloud.com/user-746329428/devcast/s-0AcVucpLys3?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV',
-}
+  motor_grafico:
+    "https://soundcloud.com/user-746329428/motores-graficos-de-jogos/s-V4WP2fkzc3s?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV",
+  design_patterns:
+    "https://soundcloud.com/user-746329428/design-patterns/s-ZvRa9qzDYei?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV",
+  direct:
+    "https://soundcloud.com/user-746329428/directcast/s-BrTV8T0D7Kl?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV",
+  dev_cast:
+    "https://soundcloud.com/user-746329428/devcast/s-0AcVucpLys3?in=user-746329428/sets/projeto-und-ii-temas-na-area-1//s-ETnYmYdYEPV",
+};
 
 http
   .createServer(async function (req, res) {
     const { url, method } = req;
-    
-    const soundToListen = sounds[url.replace('/', '')];    
-    
-    if(soundToListen) {
-      debug(`request to listen ${url}`)
-      
+
+    const soundToListen = sounds[url.replace("/", "")];
+
+    if (soundToListen) {
+      debug(`request to listen ${url}`);
+
       const audioStream = await scdl.download(soundToListen);
-    
+
       res.writeHead(200, { "Content-Type": "audio/mpeg" });
       audioStream.on("data", (data) => {
-        //debug(data);
         res.write(data);
       });
     }
